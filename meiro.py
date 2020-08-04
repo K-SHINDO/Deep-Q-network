8d14e2d9d4287f0ee09076ff2a5437bb2b8ff0d5
K-SHINDO <shindo.koki0124@gmail.com>
2020-08-04 19:06:00 +0900

    '0803update'

--- meiro.py
+++ meiro.py
@@ -34,5 +34,100 @@ plt.tick_params(axis='both',which='both',bottom='off',top='off',
 labelbottom='off',right='off',left='off',labelleft='off')
 
 # 現在地S0に緑丸を描画
-line = ax.plot([0.5], [2.5], marker="o",color='g',markersize=60)
-plt.show()
+line, = ax.plot([0.5], [2.5], marker="o",color='g',markersize=60)
+# plt.show()
+
+# 初期の方策を決定するパラメータtheta_0を設定
+
+# 行は状態0~7、列は移動方向で、上、右、下、左を表す
+theta_0 = np.array([[np.nan, 1, 1, np.nan], # S0
+                    [np.nan, 1, np.nan, 1], # S1
+                    [np.nan, np.nan, 1, 1], # S2
+                    [1, 1, 1, np.nan], # S3
+                    [np.nan, np.nan, 1, 1], # S4
+                    [1, np.nan, np.nan, np.nan], # S5
+                    [1, np.nan, np.nan, np.nan], # S6
+                    [1, 1, np.nan, np.nan], # S7
+                    ])
+
+# 方策パラメータthetaを行動方策piに変換する関数の定義
+
+def simple_convert_into_pi_from_theta(theta):
+    '''単純に割合を計算する'''
+
+    [m,n] = theta.shape # thetaの行列サイズを取得
+    pi = np.zeros((m,n))
+    for i in range(0,m):
+        pi[i, :] = theta[i, :]/np.nansum(theta[i, :]) # 割合の計算
+
+    pi = np.nan_to_num(pi) # nanを0に変換
+
+    return pi
+
+# 初期の方策pi_0を求める
+pi_0 = simple_convert_into_pi_from_theta(theta_0)
+
+# 1step移動後の状態sを求める関数を定義
+
+def get_next_s(pi, s):
+    direction = ["up","right","down","left"]
+
+    next_direction = np.random.choice(direction, p=pi[s, :])
+    # pi[s,:]の確立に従って、directionが選択される
+
+    if next_direction == "up":
+        s_next = s-3 # 上に移動するときは状態の数字が3小さくなる
+    elif next_direction == "right":
+        s_next = s+1 # 右に移動するときは状態の数字が1大きくなる
+    elif next_direction == "down":
+        s_next = s+3 # 下に移動するときは状態の数字が3大きくなる
+    elif next_direction == "left":
+        s_next = s-1 #左に移動するときは状態の数字が１小さくなる
+
+    return s_next
+
+# 迷路内をエージェントがゴールするまで移動させる関数の定義
+def goal_maze(pi):
+    s=0 # スタート地点
+    state_history = [0] #エージェントの移動を記録するリスト
+
+    while(1): # ゴールするまでループする
+        next_s = get_next_s(pi,s)
+        state_history.append(next_s)
+
+        if next_s == 8: #ゴール地点なら終了
+            break
+        else:
+            s = next_s
+
+    return state_history
+
+# 迷路内をゴールを目指して、移動
+state_history = goal_maze(pi_0)
+
+'''ゴールまでの軌跡とステップ数を表示'''
+# print(state_history)
+# print(len(state_history)-1)
+
+# エージェント移動の様子の可視化
+from matplotlib import animation
+from IPython.display import HTML
+
+def init():
+    '''背景画像の初期化'''
+    line.set_data([], [])
+    return(line,)
+
+def animate(i):
+    '''フレームごとの描画内容'''
+    state = state_history[i] #現在の場所を描く
+    x = (state % 3) + 0.5 # 状態のx座標は、3で割ったあまり+0.5
+    y = 2.5 - int(state/3) # y座標は3で割った商を2.5から引く
+    line.set_data(x,y)
+    return(line,)
+
+# 初期化関数とフレームごとの描画関数を用いて動画を作成する
+anim = animation.FuncAnimation(fig, animate, init_func=init,
+                               frames=len(state_history), interval=200,repeat=False)
+
+HTML(anim.to_jshtml())
